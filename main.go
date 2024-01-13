1ffcafec0a6cece55a4b610a088b86f2c94a7b0b
zawie <zawie@rice.edu>
2024-01-12 17:16:47 -0800

    Simple window opening

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gopxl/pixel"
+	"github.com/gopxl/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Pixel Rocks!",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	win.Clear(colornames.Skyblue)
+
+	for !win.Closed() {
+		win.Update()
+	}
+}
 
 func main() {
-    fmt.Println("Hello, world.")
+	pixelgl.Run(run)
 }
