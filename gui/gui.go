2fd1bc19990ed8bea0e49a59adbce5023c27aa93
zawie <zawie@rice.edu>
2024-01-20 16:23:11 -0800

    Refactor GUI

---
+++ gui/gui.go
@@ -0,0 +1,227 @@
+package gui
+
+import (
+	"fmt"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel/text"
+    "zawie/life/simulator/vec2"
+    "golang.org/x/image/colornames"
+	"golang.org/x/image/font/basicfont"
+	"zawie/life/simulator"
+    "image/color"
+	"time"
+)
+
+type Model interface {
+	Step()
+	Reset()
+	GetAllParticles() []*simulator.Particle
+	GetNeighborhood(pos vec2.Vector) []*simulator.Particle
+	ChunkSize() float64
+}
+
+type Mode uint8
+const (
+	PLAIN_MODE Mode = iota
+	DEBUG_MODE
+	TEMP_MODE
+)
+const DEFAULT_MODE = PLAIN_MODE
+
+
+type Gui struct {
+	cfg  pixelgl.WindowConfig;
+	window *pixelgl.Window;
+	atlas *text.Atlas;
+	maxFramesPerSecond float64;
+	mode Mode;
+	model Model;
+
+	speed uint8;
+	oldSpeed uint8;
+}
+
+var particleTypes = []color.Color{colornames.Hotpink, colornames.Limegreen, colornames.Yellow, colornames.Blue, colornames.Red}
+
+var influenceMatrix [5][5]float64
+
+func NewGui(model Model) *Gui {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Zawie's Particle Life",
+		Bounds: pixel.R(0, 0, 2000, 1000),
+		VSync:  true,
+		Maximized: true,
+		Resizable: true,
+	}
+
+    return &Gui{
+		cfg: cfg,
+		speed: 1,
+		model: model,
+		atlas: text.NewAtlas(basicfont.Face7x13, text.ASCII),
+		maxFramesPerSecond: 60,
+	}
+}
+
+func (gui *Gui) GetSize() pixel.Vec {
+	if gui.window != nil {
+		return gui.window.Bounds().Size()
+	}
+	return gui.cfg.Bounds.Size()
+}
+
+func (gui *Gui) Run() {
+	pixelgl.Run(func() {
+
+		win, err := pixelgl.NewWindow(gui.cfg)
+		if err != nil {
+			panic(err)
+		}
+		gui.window = win
+
+		for !gui.window.Closed() {
+			start := time.Now()
+			if gui.speed > 0 {
+				for i := 0; i < 1 << (gui.speed-1); i++ { 
+					gui.model.Step()
+				}
+			}	
+			gui.processInput()
+			// Sleep to ensure or FPS is no more than max
+			time.Sleep(start.Add(time.Second / time.Duration(gui.maxFramesPerSecond)).Sub(time.Now()))
+			gui.window.Clear(colornames.Black)
+			gui.render()
+			gui.window.Update()
+		}
+	})
+
+	fmt.Println("Window closed. Terminating gracefully.")
+}
+
+func (gui *Gui) render() {
+	if gui.mode == DEBUG_MODE {
+		gui.renderGrid()
+	}
+	gui.renderParticles()
+	gui.renderText()
+}
+
+func (gui *Gui) processInput() {
+		// GUI mode
+		if gui.window.JustPressed(pixelgl.KeyG) {
+			gui.toggleMode(DEBUG_MODE)
+		}
+		if gui.window.JustPressed(pixelgl.KeyT) {
+			gui.toggleMode(TEMP_MODE)
+		}
+
+		// Model reset
+		if gui.window.JustPressed(pixelgl.KeyR) {
+			gui.model.Reset()
+		}
+
+		// Speed
+		if gui.window.JustPressed(pixelgl.KeyJ) {
+			if gui.speed > 0 {
+				gui.speed--
+				if gui.speed == 0 {
+					gui.oldSpeed = 1
+				}
+			}
+		}
+		if gui.window.JustPressed(pixelgl.KeyK) {
+			if gui.speed > 0 {
+				gui.oldSpeed = gui.speed
+				gui.speed = 0
+			} else {
+				 gui.speed = gui.oldSpeed
+			}
+		}
+		if gui.window.JustPressed(pixelgl.KeyL) {
+			if gui.speed < 10 {
+				gui.speed++
+			}
+		}
+}
+
+func (gui *Gui) toggleMode(mode Mode) {
+	if gui.mode == mode {
+		gui.mode = DEFAULT_MODE
+	} else {
+		gui.mode = mode
+	}
+}
+
+func (gui *Gui) renderGrid() {
+	grid := imdraw.New(nil)
+	chunkSize := gui.model.ChunkSize()
+	grid.Color = color.RGBA{R: 15, G:15, B:15, A:0}
+	for x := 0.0; x <= gui.GetSize().X; x += chunkSize {
+		for y := 0.0; y <= gui.GetSize().Y; y += chunkSize  {
+			grid.Push(pixel.V(x,y))
+			grid.Push(pixel.V(x+chunkSize,y+chunkSize))
+			grid.Rectangle(1)
+		}
+	}
+	grid.Draw(gui.window)
+}
+
+func (gui *Gui) renderParticles() {
+	imd := imdraw.New(nil)
+	particles := gui.model.GetAllParticles()
+	for _, particle := range particles {
+		if gui.mode == TEMP_MODE {
+			imd.Color = getRatioColor(vec2.Magnitude(particle.Velocity)/0.9)
+		} else {
+			imd.Color = particle.Color
+		}
+		imd.Push(pixel.V(particle.Position.X, particle.Position.Y))
+		imd.Circle(1, 0)
+
+		if gui.mode == DEBUG_MODE && particle.Id == 0 {
+			for _, neighbor := range gui.model.GetNeighborhood(particle.Position) {
+				imd.Color = colornames.Limegreen
+				imd.Push(pixel.V(particle.Position.X, particle.Position.Y))
+				imd.Push(pixel.V(neighbor.Position.X, neighbor.Position.Y))
+				imd.Line(1)
+			}
+
+			// imd.Color = colornames.Red
+			// imd.Push(pixel.V(particle.Position.X, particle.Position.Y))
+			// imd.Circle(sim.RepulsionRadius, 1)
+
+			// imd.Color = colornames.Blue
+			// imd.Push(pixel.V(particle.Position.X, particle.Position.Y))
+			// imd.Circle(sim.ApproximationRadius, 1)
+
+			// imd.Color = colornames.White
+			// imd.Push(pixel.V(particle.Position.X, particle.Position.Y))
+			// imd.Circle(sim.InfluenceRadius, 1)
+		}
+	}
+	imd.Draw(gui.window)
+}
+
+func (gui *Gui) renderText() {
+	topLeftTxt := text.New(pixel.V(30, gui.GetSize().Y-30), gui.atlas)
+	topLeftTxt.Color = colornames.White
+	if gui.speed == 0 {
+		topLeftTxt.WriteString("PAUSED")
+	} else if gui.speed > 1 {
+		topLeftTxt.WriteString(fmt.Sprintf("SPEED x%d", 1 << (gui.speed-1)))
+	}
+	topLeftTxt.Draw(gui.window, pixel.IM)
+}
+
+func getRatioColor(ratio float64) color.RGBA {
+	// Ensure ratio is within the valid range
+	if ratio < 0 {
+		ratio = 0
+	} else if ratio > 1.0 {
+		ratio = 1.0
+	}
+	
+	return color.RGBA{uint8(ratio * 255), uint8(ratio * 255),uint8(ratio * 255), 255}
+}
--- main.go
+++ main.go
@@ -1,241 +1,46 @@
 package main
 
 import (
-	"github.com/faiface/pixel"
-	"github.com/faiface/pixel/imdraw"
-	"github.com/faiface/pixel/pixelgl"
-	"golang.org/x/image/colornames"
-	"golang.org/x/image/font/basicfont"
-	"github.com/faiface/pixel/text"
 	"zawie/life/simulator"
 	"zawie/life/simulator/vec2"
-	"image/color"
-	"time"
-	"fmt"
+	"zawie/life/gui"
 )
 
-const framesPerSecond = 60
 
-const (
-	PLAIN_MODE int = iota
-	DEBUG_MODE
-	TEMP_MODE
-)
+type modelImpl struct {
+	step  func()
+	reset func()
+
+	simulator 	*simulator.Simulator
+	gui 		*gui.Gui
+	particleCount	int
+}
+
+func (m *modelImpl) Step() { 
+	size := m.gui.GetSize()
+	m.simulator.UpdateSize(size.X, size.Y)
+	m.simulator.Step() 
+}
+func (m *modelImpl) Reset() { 
+	size := m.gui.GetSize()
+	m.simulator = simulator.NewSimulator(size.X, size.Y, m.particleCount)
+}
+func (m modelImpl) GetAllParticles() []*simulator.Particle { 
+	if m.simulator == nil {
+		panic("No simulator!!")
+	}
+	return m.simulator.GetAllParticles() 
+}
+func (m modelImpl) GetNeighborhood(pos vec2.Vector) []*simulator.Particle { return m.simulator.GetNeighborhood(pos) }
+func (m modelImpl) ChunkSize() float64 { return m.simulator.ChunkSize }
+
 
 func main() {
 
-	const X = 2000
-	const Y = 1000
-
-	fmt.Println("Creating simulator...")
-
-	particleCount :=  1000
-
-	targetId := 0
-
-	var temperatureHistory []float64
-
-	fmt.Println("Opening window...")
-	pixelgl.Run(func() {
-		mode := PLAIN_MODE
-
-		speed := 1
-		oldSpeed := speed
-
-		cfg := pixelgl.WindowConfig{
-			Title:  "Zawie's Particle Life",
-			Bounds: pixel.R(0, 0, X, Y),
-			VSync:  true,
-			Maximized: true,
-			Resizable: true,
-		}
-		win, err := pixelgl.NewWindow(cfg)
-		if err != nil {
-			panic(err)
-		}
-		atlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
-
-		sim := simulator.NewSimulator(X, Y, particleCount)
-		fmt.Println("Starting main loop...")
-		for !win.Closed() {
-			start := time.Now()
-
-			size := win.Bounds().Size()
-			if win.JustPressed(pixelgl.KeyR) {
-				sim = simulator.NewSimulator(size.X, size.Y, particleCount)
-				temperatureHistory = make([]float64, 0)
-			} else {
-				sim.UpdateSize(size.X, size.Y)
-			}
-
-			if speed > 0 {
-				for i := 0; i < 1 << (speed-1); i++ { 
-					sim.Step()
-					temperatureHistory = append(temperatureHistory, sim.ComputeAverageKineticEnergy())
-				}
-			}	
-			
-			imd := imdraw.New(nil)
-			particles := sim.GetAllParticles()
-			for _, particle := range particles {
-				if mode == TEMP_MODE {
-					imd.Color = getRatioColor(vec2.Magnitude(particle.Velocity)/0.9)
-				} else {
-					imd.Color = particle.Color
-				}
-				imd.Push(pixel.V(particle.Position.X, particle.Position.Y))
-				imd.Circle(1, 0)
-
-				if mode == DEBUG_MODE && particle.Id == targetId {
-					for _, neighbor := range sim.GetNeighborhood(particle.Position) {
-						imd.Color = colornames.Limegreen
-						imd.Push(pixel.V(particle.Position.X, particle.Position.Y))
-						imd.Push(pixel.V(neighbor.Position.X, neighbor.Position.Y))
-						imd.Line(1)
-					}
-
-					imd.Color = colornames.Red
-					imd.Push(pixel.V(particle.Position.X, particle.Position.Y))
-					imd.Circle(sim.RepulsionRadius, 1)
-
-					imd.Color = colornames.Blue
-					imd.Push(pixel.V(particle.Position.X, particle.Position.Y))
-					imd.Circle(sim.ApproximationRadius, 1)
-
-					imd.Color = colornames.White
-					imd.Push(pixel.V(particle.Position.X, particle.Position.Y))
-					imd.Circle(sim.InfluenceRadius, 1)
-				}
-			}
-
-			// Text controls
-			if win.JustPressed(pixelgl.KeyG) {
-				if mode == DEBUG_MODE {
-					mode = PLAIN_MODE
-				} else {
-					mode = DEBUG_MODE
-				}
-				targetId++
-				targetId %= 100 //TODO: Make dynamic
-			}
-
-			if win.JustPressed(pixelgl.KeyT) {
-				if mode == TEMP_MODE {
-					mode = PLAIN_MODE
-				} else {
-					mode = TEMP_MODE
-				}
-				targetId++
-				targetId %= 100 //TODO: Make dynamic
-			}
-
-			// Speed controls
-			if win.JustPressed(pixelgl.KeyL) {
-				if speed < 10 {
-					speed++
-				}
-			}
-
-			if win.JustPressed(pixelgl.KeyJ) {
-				if speed > 0 {
-					speed--
-					if speed == 0 {
-						oldSpeed = 1
-					}
-				}
-			}
-
-			if win.JustPressed(pixelgl.KeyK) {
-				if speed > 0 {
-					oldSpeed = speed
-					speed = 0
-				} else {
-					 speed = oldSpeed
-				}
-			}
-			
-			grid := imdraw.New(nil)
-			if mode == DEBUG_MODE {
-				grid.Color = color.RGBA{R: 15, G:15, B:15, A:0}
-				for x := 0.0; x <= size.X; x += float64(sim.ChunkSize) {
-					for y := 0.0; y <= size.Y; y += float64(sim.ChunkSize)  {
-						grid.Push(pixel.V(x,y))
-						grid.Push(pixel.V(x+sim.ChunkSize,y+sim.ChunkSize))
-						grid.Rectangle(1)
-					}
-				}
-			}
-
-			// Sleep to ensure we are updating grapgics at a consistent rate
-			time.Sleep(start.Add(time.Second * 1/framesPerSecond).Sub(time.Now()))
-
-			win.Clear(colornames.Black)
-			imd.Draw(win)
-			if mode == DEBUG_MODE {
-				grid.Draw(win)
-			}
-
-			topLeftTxt := text.New(pixel.V(30, size.Y-30), atlas)
-			topLeftTxt.Color = colornames.White
-			if speed == 0 {
-				topLeftTxt.WriteString("PAUSED")
-			} else if speed > 1 {
-				topLeftTxt.WriteString(fmt.Sprintf("SPEED x%d", 1 << (speed-1)))
-			}
-			topLeftTxt.Draw(win, pixel.IM)
-
-			temperatureGraph := imdraw.New(nil)
-			// Display temperature graph
-			max := 0.01
-			for _, t := range temperatureHistory {
-				if t > max {
-					max = t
-				}
-			}
-
-			graphHeight := 100.0
-			count := 0
-			agg := 0.0
-			batchSize := 50
-			x := len(temperatureHistory) - int(size.X) - batchSize
-			if x < 0 {
-				x = 0
-			}
-			for ; x < len(temperatureHistory); x++{
-				t := temperatureHistory[x]
-
-				if count <= batchSize {
-					count++
-					agg += t
-				} else {
-					agg += t - temperatureHistory[x-batchSize]
-				}
-
-				y := (agg/float64(count))/max * graphHeight
-				temperatureGraph.Color = getRatioColor((agg/float64(count))/max )
-				temperatureGraph.Push(pixel.V(float64(x-(len(temperatureHistory)-int(size.X))), float64(y)))
-				temperatureGraph.Line(2)
-				temperatureGraph.Push(pixel.V(float64(x-(len(temperatureHistory)-int(size.X))), float64(y)))
-			}
-			if mode == TEMP_MODE {
-				temperatureGraph.Draw(win)
-			}
-			win.Update()
-		}
-
-		fmt.Println("Window closed. Terminating gracefully.")
-	})
-
-}
-
-func getRatioColor(ratio float64) color.RGBA {
-	// Ensure ratio is within the valid range
-	if ratio < 0 {
-		ratio = 0
-	} else if ratio > 1.0 {
-		ratio = 1.0
+	model := &modelImpl{
+		particleCount: 1000,
 	}
-	
-	// Return the color.RGBA
-	return color.RGBA{uint8(ratio * 255), uint8(ratio * 255),uint8(ratio * 255), 255}
+	model.gui = gui.NewGui(model)
+	model.Reset()
+	model.gui.Run()
 }
